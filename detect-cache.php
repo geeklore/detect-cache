d33ce1b51571a438891df38e11779e1ce16f3e45
Shane Eckert <shaneeckert@aslan.local>
2017-10-22 15:20:52 -0700

    add license to header

--- detect-cache.php
+++ detect-cache.php
@@ -6,6 +6,12 @@
 * Author: Shane Eckert
 * Text Domain: detect-cache
 * Domain Path: /languages/
+* License: GPL-2.0+
+* License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+* Detect Cache is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 2 of the License, or any later version.
+
+* Detect Cache is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+* You should have received a copy of the GNU General Public License along with Detect Cache. If not, see http://www.gnu.org/licenses/gpl-2.0.txt.
 */
 
 if ( ! defined( 'WPINC' ) ) {
